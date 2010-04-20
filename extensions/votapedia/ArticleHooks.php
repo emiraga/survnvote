5a1ba6b91644f32776c9f5fe55ba2b1d862513a8
Emir Habul <emiraga@gmail.com>
2010-04-20 09:10:11 +0800

    Testing wgHooks in ArticleHooks.php

---
+++ extensions/votapedia/ArticleHooks.php
@@ -0,0 +1,37 @@
+<?php
+
+$wgHooks['ArticleSaveComplete'][] = 'vpOnArticleSave';
+function vpOnArticleSave($article, $user, $text, $summary)
+{
+//var_dump($article);
+//echo $user;
+echo $text;
+echo $summary;
+
+	die('saving article wtf?');
+}
+
+$wgHooks['ArticleUndelete'][] = 'vpOnArticleUndelete';
+function vpOnArticleUndelete($article, $user, $text, $summary)
+{
+//var_dump($article);
+//echo $user;
+echo $text;
+echo $summary;
+
+	die('Undelete article wtf?');
+}
+
+
+$wgHooks['ArticleDeleteComplete'][] = 'vpOnArticleDelete';
+function vpOnArticleDelete($article, $user, $text, $summary)
+{
+//var_dump($article);
+//echo $user;
+echo $text;
+echo $summary;
+
+	die('Delete article wtf?');
+}
+
+?>
